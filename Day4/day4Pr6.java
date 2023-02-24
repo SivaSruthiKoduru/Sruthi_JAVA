76e4b8f4d863e41a772507142d45fed3faa27a53
SivaSruthiKoduru <125631711+SivaSruthiKoduru@users.noreply.github.com>
2023-02-24 17:33:49 +0530

    Add files via upload

---
+++ Day4/day4Pr6.java
@@ -0,0 +1,34 @@
+package day4;
+
+import java.io.FileReader;
+public class day4Pr6 {
+	public static void main(String[] args) throws Exception {
+		
+	Deriv de = new Deriv();
+	}
+	day4Pr6() throws ArithmeticException
+	{
+		System.out.println("inside main");
+	    
+	}
+
+
+}
+class Deriv extends day4Pr6
+{
+	Deriv()
+	{
+		super();
+		System.out.println("Inside derive");
+		try
+		{
+	       //day4Pr6 d = new day4Pr6();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception caught");
+			//e.printStackTrace();
+		}
+	}
+	
+}
