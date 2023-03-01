9f3d02e8dd45d98906fdf8e7092cffcb448df560
SivaSruthiKoduru <125631711+SivaSruthiKoduru@users.noreply.github.com>
2023-03-01 07:12:51 +0530

    Add files via upload

--- Day5/Question8/PrintStringBuffer.java
+++ Day5/Question8/PrintStringBuffer.java
@@ -1,37 +1,37 @@
 package Question8;
 
 public class PrintStringBuffer extends Thread{
-	StringBuffer s;
+	StringBuffer stringbuffer;
 	public static void main(String[] args) {
-		StringBuffer s = new StringBuffer("A");
-		PrintStringBuffer d1 = new PrintStringBuffer(s);
-		PrintStringBuffer d2 = new PrintStringBuffer(s);
-		PrintStringBuffer d3 = new PrintStringBuffer(s);
-		d1.start();
-		d2.start();
-		d3.start();
+		StringBuffer stringbuffer = new StringBuffer("A");
+		PrintStringBuffer printstringbuffer1 = new PrintStringBuffer(stringbuffer);
+		PrintStringBuffer printstringbuffer2 = new PrintStringBuffer(stringbuffer);
+		PrintStringBuffer printstringbuffer3 = new PrintStringBuffer(stringbuffer);
+		printstringbuffer1.start();
+		printstringbuffer2.start();
+		printstringbuffer3.start();
 		
 		
 		
 		
 		
 	}
-	PrintStringBuffer(StringBuffer s)
+	PrintStringBuffer(StringBuffer stringbuffer)
 	{
-		this.s=s;
+		this.stringbuffer=stringbuffer;
 		
 	}
 	public void run()
 	{
-		synchronized(s)
+		synchronized(stringbuffer)
 		{
 			for(int i=0;i<100;i++)
 			{
-				System.out.print(s);
+				System.out.print(stringbuffer);
 				
 			}
 			System.out.println();
-			s.setCharAt(0,(char)(s.charAt(0)+1));
+			stringbuffer.setCharAt(0,(char)(stringbuffer.charAt(0)+1));
 		}
 	}
 
