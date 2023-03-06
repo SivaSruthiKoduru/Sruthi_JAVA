0db44e6d92bdb48b927d214c23820552b7e35bc0
SivaSruthiKoduru <125631711+SivaSruthiKoduru@users.noreply.github.com>
2023-03-06 21:55:14 +0530

    Add files via upload

--- Question14/CopyFileCharByChar.java
+++ Question14/CopyFileCharByChar.java
@@ -5,10 +5,8 @@ import java.io.FileWriter;
 
 public class CopyFileCharByChar {
 	public static void main(String[] args) {
-		try(FileReader filereader = new FileReader("myfile.txt");
-				FileWriter filewriter = new FileWriter("copied.txt");)
+		try(FileReader filereader = new FileReader("myfile.txt");FileWriter filewriter = new FileWriter("copied.txt");)
 		{
-		
 		int i;
 		while((i=filereader.read())!=-1)
 		{
